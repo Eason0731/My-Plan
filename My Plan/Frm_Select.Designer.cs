f0f93041f16d15d10a88af73714fdcaca6f8cdf2
John0731 <john.zhang0731@outlook.com>
2016-11-12 14:36:29 +0800

    Add CalDate method on Frm_Select page

--- My Plan/Frm_Select.Designer.cs
+++ My Plan/Frm_Select.Designer.cs
@@ -32,14 +32,20 @@
             this.btn_gotoAdd = new System.Windows.Forms.Button();
             this.btn_view = new System.Windows.Forms.Button();
             this.btn_exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNowDate = new System.Windows.Forms.Label();
+            this.lblCustom = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDeltaDays = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_choice
             // 
             this.btn_choice.Font = new System.Drawing.Font("宋体", 13F);
-            this.btn_choice.Location = new System.Drawing.Point(136, 74);
+            this.btn_choice.Location = new System.Drawing.Point(181, 75);
+            this.btn_choice.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_choice.Name = "btn_choice";
-            this.btn_choice.Size = new System.Drawing.Size(417, 40);
+            this.btn_choice.Size = new System.Drawing.Size(556, 50);
             this.btn_choice.TabIndex = 0;
             this.btn_choice.Text = "来做个抉择吧！";
             this.btn_choice.UseVisualStyleBackColor = true;
@@ -48,9 +54,10 @@
             // btn_gotoAdd
             // 
             this.btn_gotoAdd.Font = new System.Drawing.Font("宋体", 13F);
-            this.btn_gotoAdd.Location = new System.Drawing.Point(136, 167);
+            this.btn_gotoAdd.Location = new System.Drawing.Point(181, 185);
+            this.btn_gotoAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_gotoAdd.Name = "btn_gotoAdd";
-            this.btn_gotoAdd.Size = new System.Drawing.Size(417, 40);
+            this.btn_gotoAdd.Size = new System.Drawing.Size(556, 50);
             this.btn_gotoAdd.TabIndex = 1;
             this.btn_gotoAdd.Text = "添加点工作笔记吧";
             this.btn_gotoAdd.UseVisualStyleBackColor = true;
@@ -59,9 +66,10 @@
             // btn_view
             // 
             this.btn_view.Font = new System.Drawing.Font("宋体", 13F);
-            this.btn_view.Location = new System.Drawing.Point(136, 265);
+            this.btn_view.Location = new System.Drawing.Point(181, 292);
+            this.btn_view.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_view.Name = "btn_view";
-            this.btn_view.Size = new System.Drawing.Size(417, 40);
+            this.btn_view.Size = new System.Drawing.Size(556, 50);
             this.btn_view.TabIndex = 2;
             this.btn_view.Text = "查看工作笔记咯";
             this.btn_view.UseVisualStyleBackColor = true;
@@ -70,29 +78,90 @@
             // btn_exit
             // 
             this.btn_exit.Font = new System.Drawing.Font("宋体", 13F);
-            this.btn_exit.Location = new System.Drawing.Point(136, 362);
+            this.btn_exit.Location = new System.Drawing.Point(180, 410);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(417, 40);
+            this.btn_exit.Size = new System.Drawing.Size(556, 50);
             this.btn_exit.TabIndex = 3;
             this.btn_exit.Text = "彻底离开";
             this.btn_exit.UseVisualStyleBackColor = true;
             this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(176, 533);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 4;
+            // 
+            // lblNowDate
+            // 
+            this.lblNowDate.AutoSize = true;
+            this.lblNowDate.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold);
+            this.lblNowDate.ForeColor = System.Drawing.Color.Blue;
+            this.lblNowDate.Location = new System.Drawing.Point(269, 533);
+            this.lblNowDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNowDate.Name = "lblNowDate";
+            this.lblNowDate.Size = new System.Drawing.Size(0, 24);
+            this.lblNowDate.TabIndex = 5;
+            // 
+            // lblCustom
+            // 
+            this.lblCustom.AutoSize = true;
+            this.lblCustom.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold);
+            this.lblCustom.Location = new System.Drawing.Point(177, 580);
+            this.lblCustom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCustom.Name = "lblCustom";
+            this.lblCustom.Size = new System.Drawing.Size(0, 24);
+            this.lblCustom.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(455, 580);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 7;
+            // 
+            // lblDeltaDays
+            // 
+            this.lblDeltaDays.AutoSize = true;
+            this.lblDeltaDays.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold);
+            this.lblDeltaDays.ForeColor = System.Drawing.Color.Red;
+            this.lblDeltaDays.Location = new System.Drawing.Point(397, 580);
+            this.lblDeltaDays.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDeltaDays.Name = "lblDeltaDays";
+            this.lblDeltaDays.Size = new System.Drawing.Size(0, 24);
+            this.lblDeltaDays.TabIndex = 8;
+            // 
             // Frm_Select
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 532);
+            this.ClientSize = new System.Drawing.Size(923, 660);
             this.ControlBox = false;
+            this.Controls.Add(this.lblDeltaDays);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCustom);
+            this.Controls.Add(this.lblNowDate);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_exit);
             this.Controls.Add(this.btn_view);
             this.Controls.Add(this.btn_gotoAdd);
             this.Controls.Add(this.btn_choice);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Frm_Select";
             this.ShowIcon = false;
             this.Text = "命运的抉择";
+            this.Load += new System.EventHandler(this.Frm_Select_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,5 +171,10 @@
         private System.Windows.Forms.Button btn_gotoAdd;
         private System.Windows.Forms.Button btn_view;
         private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNowDate;
+        private System.Windows.Forms.Label lblCustom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDeltaDays;
     }
 }
--- My Plan/Frm_Select.cs
+++ My Plan/Frm_Select.cs
@@ -16,7 +16,15 @@ namespace My_Plan
             InitializeComponent();
         }
 
-        
+
+        private void Frm_Select_Load(object sender, EventArgs e)
+        {
+            label1.Text = "今天是";
+            label2.Text = "天";
+            lblNowDate.Text = DateTime.Now.ToLongDateString(); //ToLongDateString()会将日期输出为长日期字符输出
+            CalDate();
+        }
+
         private void btn_choice_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -41,6 +49,27 @@ namespace My_Plan
         private void btn_exit_Click(object sender, EventArgs e)
         {
             Environment.Exit(0); //彻底退出程序
+        }
+
+        private void CalDate()
+        {
+            DateTime TargetDate = new DateTime(2018, 8, 24); //DateTime定义三年后的日期,2018年8月24日
+            DateTime Today = DateTime.Now; //获取当前日期
+            TimeSpan delta = TargetDate - Today; //实例化TimeSpan类型对日期做一个计算
+            int deltaDays = delta.Days; //将其转化为int类型
+            if (deltaDays > 0) //距离三年期限还有多少时间的计算
+            {
+                lblCustom.Text = "距离三年期限还有";
+                lblDeltaDays.Text = deltaDays.ToString();
+            }
+
+            else { //三年期限过去的时间计算
+                //TimeSpan delta2 =  Today - TargetDate;
+                //int deltaDays2 = delta2.Days;
+                deltaDays = 0 - deltaDays;
+                lblCustom.Text = "三年期限已经过去";
+                lblDeltaDays.Text = deltaDays.ToString();
+            }
         }
 
     }
